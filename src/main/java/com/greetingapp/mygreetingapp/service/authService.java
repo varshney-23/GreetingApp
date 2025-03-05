0b73e8f81c75a6911884657f65de2ea3c8ba590a
varshney-23 <ayushivarshney40@gmail.com>
2025-03-05 14:57:58 +0530

    swagger

--- src/main/java/com/greetingapp/mygreetingapp/service/authService.java
+++ src/main/java/com/greetingapp/mygreetingapp/service/authService.java
@@ -1,6 +1,5 @@
 package com.greetingapp.mygreetingapp.service;
 
-
 import com.greetingapp.mygreetingapp.dto.authUserDTO;
 import com.greetingapp.mygreetingapp.dto.loginDTO;
 import com.greetingapp.mygreetingapp.exception.userException;
@@ -10,17 +9,17 @@ import com.greetingapp.mygreetingapp.util.EmailSenderService;
 import com.greetingapp.mygreetingapp.util.jwttoken;
 
 
+import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class authService implements IAuthService {
+public class authService implements IAuthService{
+
     @Autowired
-    userRepository userRepo;
+    userRepository authUserRepository;
 
     @Autowired
     jwttoken tokenUtil;
@@ -32,40 +31,40 @@ public class authService implements IAuthService {
 
     @Override
     public User register(authUserDTO userDTO) throws Exception {
-        try {
-            User user = new User(userDTO);
-            System.out.println(user);
-            userRepo.save(user);
+        User user = new authUser(userDTO);
 
-            String token = tokenUtil.createToken(user.getUserId());
+        String encryptedPassword = passwordEncoder.encode(userDTO.getPassword());
+        user.setPassword(encryptedPassword);
 
-            // Send email safely
-            try {
-                emailSenderService.sendEmail(
-                        user.getEmail(),
-                        "Registered in Greeting App",
-                        "Hi " + user.getFirstName() + ",\n\nYou have been successfully registered!\n\nYour details:\n\n" +
-                                "User Id: " + user.getUserId() + "\nFirst Name: " + user.getFirstName() + "\nLast Name: " + user.getLastName() +
-                                "\nEmail: " + user.getEmail() + "\nToken: " + token
-                );
-            } catch (Exception emailException) {
-                System.err.println("Error sending email: " + emailException.getMessage());
-            }
+        String token = tokenUtil.createToken(user.getUserId());
+        authUserRepository.save(user);
 
-            return user;
-        } catch (Exception e) {
-            throw new userException("Registration failed: " + e.getMessage());
-        }
+        emailSenderService.sendEmail(user.getEmail(), "Registered in Greeting App", "Hi "
+                + user.getFirstName() + ",\nYou have been successfully registered!\n\nYour registered details are:\n\n User Id:  "
+                + user.getUserId() + "\n First Name:  "
+                + user.getFirstName() + "\n Last Name:  "
+                + user.getLastName() + "\n Email:  "
+                + user.getEmail() + "\n Token:  " + token);
+
+        return user;
     }
 
 
+
     @Override
-    public String login(loginDTO logDTO){
-        Optional<com.greetingapp.mygreetingapp.model.User> user= Optional.ofNullable(userRepo.findByEmail(logDTO.getEmail()));
-        if (user.isPresent() && user.get().getPassword().equals(logDTO.getPassword()) ){
-            emailSenderService.sendEmail(user.get().getEmail(),"Logged in Successfully!", "Hii...."+user.get().getFirstName()+"\n\n You have successfully logged in into Greeting App!");
-            return "Congratulations!! You have logged in successfully!";
-        }else {
+    public String login(loginDTO logDTO) {
+        Optional<User> user = Optional.ofNullable(authUserRepository.findByEmail(logDTO.getEmail()));
+
+        if (user.isPresent()) {
+            if (passwordEncoder.matches(loginDTO.getPassword(), user.get().getPassword())) {
+                emailSenderService.sendEmail(user.get().getEmail(), "Logged in Successfully!", "Hi "
+                        + user.get().getFirstName() + ",\n\nYou have successfully logged in into Greeting App!");
+
+                return "Congratulations!! You have logged in successfully!";
+            } else {
+                throw new userException("Sorry! Email or Password is incorrect!");
+            }
+        } else {
             throw new userException("Sorry! Email or Password is incorrect!");
         }
     }
