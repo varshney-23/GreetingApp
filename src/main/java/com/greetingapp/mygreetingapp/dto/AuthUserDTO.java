0b33decfa0d711d49416273129c6772700b4206a
varshney-23 <ayushivarshney40@gmail.com>
2025-03-15 00:27:18 +0530

    hide credentials

--- src/main/java/com/greetingapp/mygreetingapp/dto/AuthUserDTO.java
+++ src/main/java/com/greetingapp/mygreetingapp/dto/AuthUserDTO.java
@@ -5,6 +5,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
+import lombok.Data;
+
+
+@Data
 public class AuthUserDTO {
 
     @NotNull(message = "First name is required")
@@ -24,36 +28,4 @@ public class AuthUserDTO {
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%?&])[A-Za-z\\d@$!%?&]{8,}$",
             message = "Password must be at least 8 characters long, contain an uppercase letter, a lowercase letter, a number, and a special character")
     private String password;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
